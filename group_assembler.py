297fcf54c4ba39ddcd8d1930e5ac10e5f1fefd18
Christopher Pourier <corez92@users.noreply.github.com>
2016-10-10 09:29:38 -0400

    no overflow case

--- group_assembler.py
+++ group_assembler.py
@@ -2,18 +2,24 @@ import random
 
 participants = []
 
+participants = input("Please enter a list with names of participants (write this like you would in Python: ['Chris','Max','John'])")
+
 group_size = 3
 overflow = 'n'
 # Check to see if possible to make well-balanced groups
+# Assume overflow never happens
+# 10/10/16
+# while overflow == 'n':
+#     if len(participants) % int(group_size) != 0:
+#             overflow = input("There are {} participants over, do you want to add them to another group? Yes = y, No = n:\n".format(str(len(participants) % int(group_size))))
+#             if overflow == "y":
+#                 break
+#             group_size = input("How big should the new groups be?".format(str(len(participants) % int(group_size))))
+#     else:
+#         overflow = 'resolved in while loop'
 
-while overflow == 'n':
-    if len(participants) % int(group_size) != 0:
-            overflow = input("There are {} participants over, do you want to add them to another group? Yes = y, No = n:\n".format(str(len(participants) % int(group_size))))
-            if overflow == "y":
-                break
-            group_size = input("How big should the new groups be?".format(str(len(participants) % int(group_size))))
-    else:
-        overflow = 'resolved in while loop'
+# 10/10/16
+
 #group =
 ##groups = [participants[j] for i in range(len(participants) // int(group_size) + 1) for j in range(group_size)]
 ##
@@ -31,8 +37,8 @@ while overflow == 'n':
 ##        print(choices)
 ##        #print(choices)
 
-random.shuffle(participants)
-group1 = random.sample(participants,  group_size)
-groups = [random.sample(participants,  group_size) for h in range(len(participants) // int(group_size) + 1)]
-groups[random.choice(len(groups))].append
+##random.shuffle(participants)
+##group1 = random.sample(participants,  group_size)
+##groups = [random.sample(participants,  group_size) for h in range(len(participants) // int(group_size) + 1)]
+##groups[random.choice(len(groups))].append
         
